2a5541d45b46ea6a48da718084be50601308c872
outnicky <nicolas_hugo2001@yahoo.com>
2026-01-31 06:58:14 -0300

    aaaaaaaa

--- scripts/envy.gd
+++ scripts/envy.gd
@@ -3,26 +3,40 @@ class_name envy extends EnemyClass
 
 @onready var animation_player: AnimatedSprite2D= $AnimatedSprite2D
 
-
+var shoot_range = 500
+var bullet_size = 100
+var target_distance = 0
+var traveled_distance = 0
+@onready var attack = $Attack
+func shoot(direction:Vector2):
+	if state == State.Attacking:
+		return
+	set_state(State.Attacking)
+	var ray :RayCast2D = $Attack/RayCast2D
+	var collision = ray.get_collider()
+	if not collision:
+		target_distance = 500
+	else:
+		var vec = collision.position - attack.position
+		target_distance = vec.x
+	var line = Line2D.new()
+	line.width = 2
+	attack.add_child(line)
+	line.add_point(Vector2(traveled_distance, 0))
+	traveled_distance +=bullet_size
+	if traveled_distance > target_distance:
+		traveled_distance = target_distance
+		line.add_point(Vector2(traveled_distance ))
+		set_state(State.Chasing)
+		
+	
 
 func _process(delta: float) -> void:
 	flip_character()
+	shoot(Vector2(1,0))
+	
 
-func set_state(new_state: State):
-	if state == new_state:
-		return
-	if new_state == State.Idle:
-		animation_player.play("Idle")
-		
-		state = State.Idle	
-	if new_state == State.Wandering:
-		animation_player.play("Walk")
 
-		state = State.Wandering
-	if new_state == State.Chasing:
-		animation_player.play("Attack")
-		state = State.Chasing
-		
 
 
 
